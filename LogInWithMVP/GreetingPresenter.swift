81033d378d832961c655406bad76a2215587f8d3
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-05-12 23:01:49 +0500

    1. In MainViewController created MainViewProtocol with func setGreeting for adding text of greeting to greetingLabel. 2. Created swift file - GreetingPresenter.

---
+++ LogInWithMVP/GreetingPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  GreetingPresenter.swift
+//  LogInWithMVP
+//
+//  Created by Artem Pavlov on 12.05.2022.
+//
+
+import Foundation
---
+++ LogInWithMVP/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  LogInWithMVP
+//
+//  Created by Artem Pavlov on 12.05.2022.
+//
+
+import UIKit
+
+protocol MainViewProtocol {
+    func setGreeting(_ greeting: String)
+}
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func enterButton() {
+    }
+    
+}
+
+extension MainViewController: MainViewProtocol {
+    func setGreeting(_ greeting: String) {
+        greetingLabel.text = greeting
+    }
+    
+    
+}
+
